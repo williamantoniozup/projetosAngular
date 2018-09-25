9dd827f4c6862be5705cba9f843bbba84a279767
William <william.antonio@zup.com.br>
2018-09-25 17:37:49 -0300

    aula 04 finish

---
+++ hello-typescript/main.ts
@@ -0,0 +1,38 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y) {
+    return x + y;
+}
+
+// ES6 ou ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 4);
+
+class Matematica {
+    soma(x,y){
+        return x+y;
+    }
+}
+
+var n1: string = 'asdasa';  // --> tipagem de variavel;
+var n2 = 4;
+
+
+
+
+
+
+/*
+ ECMAS2015 ==> transpile, compilar o ES6 em vanilla puro
+
+    to transpile -> tsc namefile.ts
+
+
+ */
--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeuPrimeiroComponent 
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component 
   ],
   imports: [
     BrowserModule
