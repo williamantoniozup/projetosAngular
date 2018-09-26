d37cafbb350e62d61490219d37adfc62212635b8
William <william.antonio@zup.com.br>
2018-09-26 15:49:17 -0300

    updates

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  mensagem: String = 'Testando property binding';
+  urlImage: string = 'https://www.4devs.com.br/4devs_gerador_imagem.php?acao=gerar_imagem&txt_largura=320&txt_altura=240&extensao=png&fundo_r=0.06274509803921569&fundo_g=0.996078431372549&fundo_b=0.9568627450980393&texto_r=0&texto_g=0&texto_b=0&texto=Gerador%20Imagem%20%234Devs&tamanho_fonte=10';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
   }
 
 }
