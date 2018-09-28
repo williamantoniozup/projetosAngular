f500b4566d26cf6512b557e4afbe5e388165ede8
William <william.antonio@zup.com.br>
2018-09-28 15:50:46 -0300

    updates

--- angular-cli-libs-externas/src/app/app.module.ts
+++ angular-cli-libs-externas/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TesteComponent } from './teste/teste.component';
-import { TesteeComponent } from './testee/testee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TesteComponent,
-    TesteeComponent
   ],
   imports: [
     BrowserModule
