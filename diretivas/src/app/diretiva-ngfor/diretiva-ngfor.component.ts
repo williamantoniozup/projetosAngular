7a2a766f10a6c085f9d1935cf98b9eb91341c0cf
William <william.antonio@zup.com.br>
2018-10-01 15:18:11 -0300

    updates ngFor

--- diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
+++ diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgforComponent implements OnInit {
 
+  cursos: string[] = ['Angular 2', 'Java', 'Python'];
+
   constructor() { }
 
   ngOnInit() {
