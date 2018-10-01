9f3d02d9213028826040b16c457a49335edc9bf6
William <william.antonio@zup.com.br>
2018-10-01 15:05:41 -0300

    updates

---
+++ diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngfor',
+  templateUrl: './diretiva-ngfor.component.html',
+  styleUrls: ['./diretiva-ngfor.component.css']
+})
+export class DiretivaNgforComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
