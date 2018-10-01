b06fbb1c124fb5a716d1c04f8429a73ada416ab7
William <william.antonio@zup.com.br>
2018-10-01 10:44:05 -0300

    updates

--- angular-cli-libs-externas/src/app/app.module.ts
+++ angular-cli-libs-externas/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BsDropdownModule.forRoot(),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
