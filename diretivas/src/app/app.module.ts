c4cfd33ee273f4c17aa4463d157d4dd9cf660f10
unknown <william.antonio@zup.com.br>
2018-10-10 10:39:41 -0300

    diretivas add

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { TesteComponent } from './teste/teste.component';
 import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
 import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-content.component';
+import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
+import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 
 
 @NgModule({
@@ -24,7 +26,9 @@ import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-conte
     TesteComponent,
     DiretivaNgstyleComponent,
     OperadorElvisComponent,
-    ExemploNgContentComponent
+    ExemploNgContentComponent,
+    FundoAmareloDirective,
+    DiretivasCustomizadasComponent
   ],
   imports: [
     BrowserModule,
