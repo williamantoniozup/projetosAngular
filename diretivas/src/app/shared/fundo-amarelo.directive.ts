67af4d942520ac04613fcaead9bf684f68ea4125
unknown <william.antonio@zup.com.br>
2018-10-10 14:48:08 -0300

    updates

--- diretivas/src/app/shared/fundo-amarelo.directive.ts
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { element } from 'protractor';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
-  selector: '[FundoAmarelo]'
+  selector: 'p[FundoAmarelo]' // p -> significa que vai aplicar somente em paragrafos.
 })
 export class FundoAmareloDirective {
 
-  constructor() { }
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer) {
+    //console.log(_elementRef);
+    //this._elementRef.nativeElement.style.backgroundColor = 'yellow'; // evitar usar desta maneira.
+    this._renderer.setElementStyle(
+      this._elementRef.nativeElement,
+      'backgroundColor',
+      'yellow'
+      );
+  }
 
 }
